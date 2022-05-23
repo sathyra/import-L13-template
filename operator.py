040f85157b20ab40c8ceca144fb9524a34b0f3c3
sathyra <105861021+sathyra@users.noreply.github.com>
2022-05-23 17:18:05 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,17 @@
+x = 10
+y = 20
+
+if x > 0 and y > 0:
+    print('Both x and y are positive numbers')
+
+# or operator:
+
+x = 10
+y = 20
+
+if x > 0 or y > 0:
+    print('At least one of x and y is positive number')
+
+a = 37
+if not (a % 3 == 0 or a % 4 == 0):
+    print('37 is not divisible by either 3 or 4')
